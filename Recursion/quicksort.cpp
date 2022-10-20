9fe49ae5814c87e26bcec66e68d785556d092c9e
unknown <vimarshjain321@gmail.com>
2022-10-20 13:31:01 +0530

    Contributed in quick sort problem

--- Recursion/quicksort.cpp
+++ Recursion/quicksort.cpp
@@ -1,72 +1,61 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
-  
-// A utility function to swap two elements
-void swap(int* a, int* b)
+ 
+int partition(int arr[], int start, int end)
 {
-    int t = *a;
-    *a = *b;
-    *b = t;
-}
-  
-/* This function takes last element as pivot, places
-the pivot element at its correct position in sorted
-array, and places all smaller (smaller than pivot)
-to left of pivot and all greater elements to right
-of pivot */
-int partition(int arr[], int low, int high)
-{
-    int pivot = arr[high]; // pivot
-    int i
-        = (low
-           - 1); // Index of smaller element and indicates
-                 // the right position of pivot found so far
-  
-    for (int j = low; j <= high - 1; j++) {
-        // If current element is smaller than the pivot
-        if (arr[j] < pivot) {
-            i++; // increment index of smaller element
-            swap(&arr[i], &arr[j]);
+ 
+    int pivot = arr[start];
+ 
+    int count = 0;
+    for (int i = start + 1; i <= end; i++) {
+        if (arr[i] <= pivot)
+            count++;
+    }
+    int pivotIndex = start + count;
+    swap(arr[pivotIndex], arr[start]);
+    int i = start, j = end;
+ 
+    while (i < pivotIndex && j > pivotIndex) {
+ 
+        while (arr[i] <= pivot) {
+            i++;
+        }
+ 
+        while (arr[j] > pivot) {
+            j--;
+        }
+ 
+        if (i < pivotIndex && j > pivotIndex) {
+            swap(arr[i++], arr[j--]);
         }
     }
-    swap(&arr[i + 1], &arr[high]);
-    return (i + 1);
+ 
+    return pivotIndex;
 }
-  
-/* The main function that implements QuickSort
-arr[] --> Array to be sorted,
-low --> Starting index,
-high --> Ending index */
-void quickSort(int arr[], int low, int high)
+ 
+void quickSort(int arr[], int start, int end)
 {
-    if (low < high) {
-        /* pi is partitioning index, arr[p] is now
-        at right place */
-        int pi = partition(arr, low, high);
-  
-        // Separately sort elements before
-        // partition and after partition
-        quickSort(arr, low, pi - 1);
-        quickSort(arr, pi + 1, high);
-    }
+    if (start >= end)
+        return;
+
+    int p = partition(arr, start, end);
+
+    quickSort(arr, start, p - 1);
+
+    quickSort(arr, p + 1, end);
 }
-  
-/* Function to print an array */
-void printArray(int arr[], int size)
-{
-    int i;
-    for (i = 0; i < size; i++)
-        cout << arr[i] << " ";
-    cout << endl;
-}
-  
-// Driver Code
+ 
 int main()
 {
-    int arr[] = { 10, 7, 8, 9, 1, 5 };
-    int n = sizeof(arr) / sizeof(arr[0]);
+ 
+    int arr[] = { 9, 3, 4, 2, 1, 8 };
+    int n = 6;
+ 
     quickSort(arr, 0, n - 1);
-    cout << "Sorted array: \n";
-    printArray(arr, n);
+ 
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+ 
     return 0;
 }
