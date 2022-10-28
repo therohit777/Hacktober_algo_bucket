ef3685ebcaf52fe4837c7863a88e349ed2737643
rishhiiii <87380598+rishhiiii@users.noreply.github.com>
2022-10-28 23:18:42 +0530

    Update Reverse_Queue.cpp

--- Stacks and Queues/Reverse_Queue.cpp
+++ Stacks and Queues/Reverse_Queue.cpp
@@ -1,78 +1,48 @@
-/*
-You have been given a queue that can store integers as the data. You are required to write a function that reverses the populated queue itself without using any other data structures.
-Input Format:
-The first list of input contains an integer 't' denoting the number of test cases/queries to be run.
-Then the test cases follow.
-
-The first line input for each test case/query contains an integer N, denoting the total number of elements in the queue.
-
-The second line of input contains N integers separated by a single space, representing the order in which the elements are enqueued into the queue.
-Output Format:
-For each test case/query, the only line of output prints the order in which the queue elements are dequeued, all of them separated by a single space.
-
-Output for every test case/query will be printed on a new line. 
-Note:
-You are not required to print the expected output explicitly, it has already been taken care of. Just make the changes in the input queue itself.
-Constraints:
-1 <= t <= 100
-1 <= N <= 10^4
--2^31 <= data <= 2^31 - 1
-
-Time Limit: 1sec 
-Sample Input 1:
-1
-6
-1 2 3 4 5 10
-Note:
-Here, 1 is at the front and 10 is at the rear of the queue.
-Sample Output 1:
-10 5 4 3 2 1
-Sample Input 2:
-2
-5
-2 8 15 1 10
-3
-10 20 30
-Sample Output 2:
-10 1 15 8 2 
-30 20 10 
-*/
-#include <iostream>
-#include <stack>
-#include <queue>
+// C++ code for reversing a queue
+#include <bits/stdc++.h>
 using namespace std;
 
-void reverseQueue(queue<int> &input) {
-	// Write your code here
-   	if(input.empty()){
-        return;
-    }
-    int temp = input.front();
-    input.pop();
-    reverseQueue(input);
-    input.push(temp);
+// Utility function to print the queue
+void printQueue(queue<long long int> Queue)
+{
+	while (!Queue.empty()) {
+		cout << Queue.front() << " ";
+		Queue.pop();
+	}
 }
 
-int main() {
-    int t;
-    cin >> t;
+// Recursive function to reverse the queue
+void reverseQueue(queue<long long int>& q)
+{
+	// Base case
+	if (q.empty())
+		return;
 
-    while (t--) {
-        queue<int> q;
-        int size;
-        cin >> size;
+	// Dequeue current item (from front)
+	long long int data = q.front();
+	q.pop();
 
-        for (int i = 0, val; i < size; i++) {
-            cin >> val;
-            q.push(val);
-        }
+	// Reverse remaining queue
+	reverseQueue(q);
 
-        reverseQueue(q);
-        while (!q.empty()) {
-            cout << q.front() << " ";
-            q.pop();
-        }
+	// Enqueue current item (to rear)
+	q.push(data);
+}
 
-        cout << "\n";
-    }
+// Driver code
+int main()
+{
+	queue<long long int> Queue;
+	Queue.push(56);
+	Queue.push(27);
+	Queue.push(30);
+	Queue.push(45);
+	Queue.push(85);
+	Queue.push(92);
+	Queue.push(58);
+	Queue.push(80);
+	Queue.push(90);
+	Queue.push(100);
+	reverseQueue(Queue);
+	printQueue(Queue);
 }
